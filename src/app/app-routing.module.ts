201802444201b26a277ab0f472e63f5495b49fa1
swarupe <swarup.e1998@gmail.com>
2018-11-01 21:19:29 +0530

    Added form fields in master

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AdminComponent } from './components/admin/admin.component';
+import { MastersComponent } from './components/home/masters/masters.component';
+import { ReportsComponent } from './components/home/reports/reports.component';
+import { AcademicYearComponent } from './components/home/masters/academic-year/academic-year.component';
+import { CollegeComponent } from './components/home/masters/college/college.component';
+import { DistrictComponent } from './components/home/masters/district/district.component';
+import { FinancialYearComponent } from './components/home/masters/financial-year/financial-year.component';
+import { IncomeHeadsComponent } from './components/home/masters/income-heads/income-heads.component';
+import { UniversityComponent } from './components/home/masters/university/university.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'admin', component: AdminComponent},
+  {path: 'masters', component: MastersComponent},
+  {path: 'reports', component: ReportsComponent},
+  {path: 'masters/academic_year', component: AcademicYearComponent},
+  {path: 'masters/college', component: CollegeComponent},
+  {path: 'masters/district', component: DistrictComponent},
+  {path: 'masters/financial_year', component: FinancialYearComponent},
+  {path: 'masters/income_heads', component: IncomeHeadsComponent},
+  {path: 'masters/university', component: UniversityComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
 
@@ -14,3 +30,16 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const ModulesList = [
+  HomeComponent,
+  MastersComponent,
+  AcademicYearComponent,
+  FinancialYearComponent,
+  UniversityComponent,
+  DistrictComponent,
+  CollegeComponent,
+  IncomeHeadsComponent,
+  AdminComponent,
+  ReportsComponent
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, ModulesList } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { HomeComponent } from './components/home/home.component';
-import { MastersComponent } from './components/masters/masters.component';
-import { AcademicYearComponent } from './components/masters/academic-year/academic-year.component';
-import { FinancialYearComponent } from './components/masters/financial-year/financial-year.component';
-import { UniversityComponent } from './components/masters/university/university.component';
-import { DistrictComponent } from './components/masters/district/district.component';
-import { CollegeComponent } from './components/masters/college/college.component';
-import { IncomeHeadsComponent } from './components/masters/income-heads/income-heads.component';
-import { AdminComponent } from './components/admin/admin.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent,
-    MastersComponent,
-    AcademicYearComponent,
-    FinancialYearComponent,
-    UniversityComponent,
-    DistrictComponent,
-    CollegeComponent,
-    IncomeHeadsComponent,
-    AdminComponent
+    ModulesList
   ],
   imports: [
     BrowserModule,
